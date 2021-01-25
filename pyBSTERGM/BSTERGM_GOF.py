9a7028c2c1676ff4862698d92750cb8304845261
Seokjun Choi <Choi.Seokjun@outlook.com>
2021-01-26 05:32:57 +0900

    pyBSTERGM gof and diagnosis module

---
+++ pyBSTERGM/BSTERGM_GOF.py
@@ -0,0 +1,167 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from network import UndirectedNetwork
+from network_sampler import NetworkSampler
+from BSTERGM import BSTERGM
+
+class BSTERGM_GOF:
+    def __init__(self, model_function, GOFplot_netstat_function, init_network, posterior_formation, posterior_dissolution, rng_seed=2021):
+        self.model = model_function
+        self.netstats = GOFplot_netstat_function
+        self.formation_samples = posterior_formation
+        self.dissolution_samples = posterior_dissolution
+        self.init_network = init_network
+
+        self.random_seed = rng_seed
+        self.random_gen = np.random.default_rng(seed=rng_seed)
+        self.netstat_list = []
+
+    def choose_samples(self):
+        num_posterior_sample = len(self.formation_samples)
+        sample_index = self.random_gen.integers(num_posterior_sample)
+        return self.formation_samples[sample_index], self.dissolution_samples[sample_index]
+    
+    def get_next_network(self, last_net, formation_net, dissolution_net):
+        dissolution_net_structure = dissolution_net.structure
+        y_last_minus_ydis_structure = last_net.structure.copy()
+        y_now_structure = formation_net.structure.copy()
+        node_num = last_net.node_num
+
+        for row in range(node_num):
+            for col in range(node_num):
+                if dissolution_net_structure[row,col]==1:
+                    y_last_minus_ydis_structure[row,col]=0
+        
+        for row in range(node_num):
+            for col in range(node_num):
+                if y_last_minus_ydis_structure[row,col]==1:
+                    y_now_structure[row,col] = 0
+        
+        return UndirectedNetwork(y_now_structure)
+
+
+    def gof_sampler(self, formation_parameter, dissolution_parameter, exchange_iter, rng_seed):
+        Net_formation_sampler = NetworkSampler(self.model, formation_parameter, self.init_network, rng_seed)
+        Net_dissolution_sampler = NetworkSampler(self.model, dissolution_parameter, self.init_network, rng_seed)
+        
+        Net_formation_sampler.run(exchange_iter)
+        Net_dissolution_sampler.run(exchange_iter)
+
+        return (Net_formation_sampler.network_samples[-1], Net_dissolution_sampler.network_samples[-1])
+
+    def gof_run(self, num_sim, exchange_iter):
+        for i in range(num_sim):
+            if i%10==9:
+                print("gof iter: ",i+1)
+            formation_parameter, dissolution_parameter = self.choose_samples()
+            formation_net, dissolution_net = self.gof_sampler(formation_parameter, dissolution_parameter, exchange_iter, self.random_seed + i)
+            gof_sample_network = self.get_next_network(self.init_network, formation_net, dissolution_net)
+            self.netstat_list.append(self.netstats(gof_sample_network))
+    
+    def netstat_trace(self):
+        netstat_array = np.array(self.netstat_list)
+        return netstat_array.T.tolist()
+
+
+    def show_boxplot(self, next_net=None, show=True):
+        netstats = self.netstat_trace()
+        plt.boxplot(netstats)
+
+        if next_net is not None:
+            line_val = self.netstats(next_net)
+            plt.plot([i+1 for i in range(len(line_val))], line_val, '-b')
+
+        if show:
+            plt.show()
+
+
+
+
+
+
+if __name__ == "__main__":
+    test_structure1 = np.array(
+    [
+        [0,1,1,0,0,0,0,0,0,0],
+        [1,0,1,1,0,0,0,0,0,0],
+        [1,1,0,1,0,0,0,0,0,0],
+        [0,1,1,0,1,0,0,0,0,0],
+        [0,0,0,1,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0]
+    ]
+    )
+    test_structure2 = np.array(
+    [
+        [0,1,1,0,0,0,0,0,0,0],
+        [1,0,1,1,0,0,0,0,0,0],
+        [1,1,0,1,0,0,0,0,0,0],
+        [0,1,1,0,1,1,0,0,0,0],
+        [0,0,0,1,0,0,0,0,0,0],
+        [0,0,0,1,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,1,1,0],
+        [0,0,0,0,0,0,1,0,0,1],
+        [0,0,0,0,0,0,1,0,0,1],
+        [0,0,0,0,0,0,0,1,1,0]
+    ]
+    )
+    test_structure3 = np.array(
+    [
+        [0,0,1,0,0,0,0,0,1,1],
+        [0,0,1,1,0,0,0,0,1,0],
+        [1,1,0,0,0,0,0,1,0,0],
+        [0,1,0,0,1,1,0,0,0,0],
+        [0,0,0,1,0,0,0,0,0,0],
+        [0,0,0,1,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,1,1,0],
+        [0,0,1,0,0,0,1,0,0,1],
+        [1,1,0,0,0,0,1,0,0,1],
+        [1,0,0,0,0,0,0,1,1,0]
+    ]
+    )
+
+    test_initnet1 = UndirectedNetwork(test_structure1)
+    test_initnet2 = UndirectedNetwork(test_structure2)
+    test_initnet3 = UndirectedNetwork(test_structure3)
+    test_obs_seq = [test_initnet1, test_initnet2, test_initnet3]
+
+
+    def model_netStat(network : UndirectedNetwork):
+        model = []
+        #define model
+        model.append(network.statCal_edgeNum())
+        model.append(network.statCal_geoWeightedESP(0.5))
+        return np.array(model)
+
+    def gof_netStat(network: UndirectedNetwork):
+        gof_netstat = []
+        node_degree_dist = network.statCal_nodeDegreeDist()
+        ESP_dist = network.statCal_EdgewiseSharedPartnerDist()
+
+        for val in node_degree_dist:
+            gof_netstat.append(val)
+        for val in ESP_dist:
+            gof_netstat.append(val)
+        return np.array(gof_netstat)
+
+
+    initial_formation_param = np.array([0.1, 0.1])
+    initial_dissolution_param = np.array([0.1, 0.1])
+    test_BSTERGM_sampler = BSTERGM(model_netStat, initial_formation_param, initial_dissolution_param, test_obs_seq, 2021)
+    test_BSTERGM_sampler.run(3000, exchange_iter=50)
+
+
+    gof_inst1 = BSTERGM_GOF(model_netStat, gof_netStat, test_initnet1, 
+        test_BSTERGM_sampler.MC_formation_samples[-2000::10], test_BSTERGM_sampler.MC_dissolution_samples[-2000::10])
+    gof_inst1.gof_run(num_sim=100, exchange_iter=200)
+    gof_inst1.show_boxplot(next_net=test_initnet2)
+
+
+    gof_inst2 = BSTERGM_GOF(model_netStat, gof_netStat, test_initnet2, 
+        test_BSTERGM_sampler.MC_formation_samples[-2000::10], test_BSTERGM_sampler.MC_dissolution_samples[-2000::10])
+    gof_inst2.gof_run(num_sim=100, exchange_iter=200)
+    gof_inst2.show_boxplot(next_net=test_initnet3)
---
+++ pyBSTERGM/BSTERGM_diagnosis.py
@@ -0,0 +1,137 @@
+import csv
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from BSTERGM import BSTERGM
+
+class BSTERGM_posterior_work:
+    def __init__(self):
+        self.initial_formation_param = np.array(0)
+        self.initial_dissolution_param = np.array(0)
+        self.MC_formation_samples = []
+        self.MC_dissolution_samples = []
+
+    def read_from_csv(self, file_name, formation_param_dim, dissolution_param_dim):
+        '''caution:
+        Returned BSTERGM_Read instance has only propoerties as following:
+            self.initial_formation_param(if the sample is cut burn-in, this value may not the real initial value.)
+            self.initial_dissolution_param(same as above)
+            self.MC_formation_samples
+            self.MC_dissolution_samples
+        thus, DO NOT use this instance to rerun MCMC or other works,
+        except getting plots or summary statistics, diagonostics, GOF, etc.
+        '''
+        with open("pyBSTERGM/" + file_name + '.csv', 'r', newline='', encoding='utf-8') as csvfile:
+            reader = csv.reader(csvfile)
+            for csv_row in reader:
+                csv_row = [float(elem) for elem in csv_row]
+                self.MC_formation_samples.append(np.array(csv_row[0:formation_param_dim]))
+                self.MC_dissolution_samples.append(np.array(csv_row[formation_param_dim : formation_param_dim + dissolution_param_dim]))
+
+        self.initial_formation_param = self.MC_formation_samples[0]
+        self.initial_dissolution_param = self.MC_dissolution_samples[0]
+
+    def read_from_BSTERGM_instance(self):
+        pass
+    
+    def MC_sample_trace(self):
+        formation_trace = []
+        dissolution_trace = []
+        
+        for _ in range(len(self.initial_formation_param)):
+            formation_trace.append([])
+        for _ in range(len(self.initial_dissolution_param)):
+            dissolution_trace.append([])
+        for formation_sample in self.MC_formation_samples:
+            for i, param_val in enumerate(formation_sample):
+                formation_trace[i].append(param_val)
+        for dissolution_sample in self.MC_dissolution_samples:
+            for i, param_val in enumerate(dissolution_sample):
+                dissolution_trace[i].append(param_val)
+        return formation_trace, dissolution_trace
+
+
+    def show_traceplot(self, show=True):
+        formation_trace, dissolution_trace = self.MC_sample_trace()
+        grid_column = 1
+        # grid_row = int(len(netStat)/2+0.51)
+        grid_row = len(self.initial_formation_param) + len(self.initial_dissolution_param)
+        plt.figure(figsize=(5*grid_column, 3*grid_row))
+        for i, paramSeq in enumerate(formation_trace):
+            plt.subplot(grid_row, grid_column, i+1)
+            plt.plot(range(len(paramSeq)), paramSeq)
+        for i, paramSeq in enumerate(dissolution_trace):
+            plt.subplot(grid_row, grid_column, len(self.initial_formation_param)+i+1)
+            plt.plot(range(len(paramSeq)), paramSeq)
+
+        if show:
+            plt.show()
+
+    def show_histogram(self, bins=100 ,show=True):
+        formation_trace, dissolution_trace = self.MC_sample_trace()
+        grid_column = 1
+        # grid_row = int(len(netStat)/2+0.51)
+        grid_row = len(self.initial_formation_param) + len(self.initial_dissolution_param)
+        plt.figure(figsize=(5*grid_column, 3*grid_row))
+        for i, paramSeq in enumerate(formation_trace):
+            plt.subplot(grid_row, grid_column, i+1)
+            plt.hist(paramSeq, bins=bins, density=True)
+        for i, paramSeq in enumerate(dissolution_trace):
+            plt.subplot(grid_row, grid_column, len(self.initial_formation_param)+i+1)
+            plt.hist(paramSeq, bins=bins, density=True)
+
+        if show:
+            plt.show()
+
+    def get_autocorr(self, trace, maxLag):
+        acf = []
+        trace_mean = np.mean(trace)
+        trace = [elem - trace_mean  for elem in trace]
+        n_var = sum([elem**2 for elem in trace])
+        for k in range(maxLag+1):
+            N = len(trace)-k
+            n_cov_term = 0
+            for i in range(N):
+                n_cov_term += trace[i]*trace[i+k]
+            acf.append(n_cov_term / n_var)
+        return acf
+
+    def show_acfplot(self, maxLag=50, show=True):
+        formation_trace, dissolution_trace = self.MC_sample_trace()
+        grid_column = 1
+        # grid_row = int(len(netStat)/2+0.51)
+        grid_row = len(self.initial_formation_param) + len(self.initial_dissolution_param)
+        grid = [i for i in range(maxLag+1)]
+
+        plt.figure(figsize=(5*grid_column, 3*grid_row))
+        for i, paramSeq in enumerate(formation_trace):
+            plt.subplot(grid_row, grid_column, i+1)
+            nowseq_acf = self.get_autocorr(paramSeq, maxLag)
+            plt.ylim([-1,1])
+            plt.bar(grid, nowseq_acf, width=0.3)
+            plt.axhline(0, color="black", linewidth=0.8)
+
+        for i, paramSeq in enumerate(dissolution_trace):
+            plt.subplot(grid_row, grid_column, len(self.initial_formation_param)+i+1)
+            nowseq_acf = self.get_autocorr(paramSeq, maxLag)
+            plt.ylim([-1,1])
+            plt.bar(grid, nowseq_acf, width=0.3)
+            plt.axhline(0, color="black", linewidth=0.8)
+
+        if show:
+            plt.show()
+
+
+
+
+
+if __name__ == "__main__":
+    reader_inst = BSTERGM_posterior_work()
+    reader_inst.read_from_csv("results/seq1_0chain", 2, 2)
+    # print(reader_inst.MC_formation_samples[0:10])
+    # reader_inst.show_traceplot()
+    # reader_inst.show_histogram()
+
+    reader_inst.show_acfplot()
+
